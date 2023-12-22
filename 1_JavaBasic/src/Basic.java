b4a230123b62c4b10526c32e24b354ed872879c6
kimchangheon <kimchangheon73@gmail.com>
2023-12-22 17:32:58 +0900

    12-22

--- 1_JavaBasic/src/Basic.java
+++ 1_JavaBasic/src/Basic.java
@@ -137,6 +137,31 @@ public class Basic {
 		}
 	}
 	
+	// 2.11 switch-weather
+	public static void swtich_weather() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("월(1~12)을 입력하시오:");
+		int month = scanner.nextInt(); // 정수로 월 입력
+		
+		switch(month) {
+		case 3: case 4: case 5:
+			System.out.println("봄입니다");
+			break;
+		case 6: case 7: case 8:
+			System.out.println("여름입니다");
+			break;	
+		case 9: case 10: case 11:
+			System.out.println("가을입니다");
+			break;
+		case 12: case 1: case 2:
+			System.out.println("겨울입니다");
+			break;
+		default:
+			System.out.println("잘못된 입력");
+		}
+
+	}
+	
 	
 	
 	
@@ -151,7 +176,8 @@ public class Basic {
 		// ifcalc();
 		// ifcmd();
 		// multi_if();
-		switch_case();
+//		switch_case();
+		swtich_weather();
 	}
 }
 
